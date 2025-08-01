b59f8d61e458516ea6bdabcb5097f82cfcfb9e2b
hermonbest <heri2024top@gmail.com>
2025-08-01 14:09:48 -0700

    Expose CSRF endpoint at /api/auth/csrf/

--- backend/freelance_project/urls.py
+++ backend/freelance_project/urls.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
+from django.middleware.csrf import get_token
 
+
+def csrf(request):
+    return JsonResponse({'message': 'CSRF cookie set', 'csrfToken': get_token(request)})
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/auth/csrf/', csrf, name='csrf'),
     path('api/auth/', include('users.urls')),
     path('api/jobs/', include('jobs.urls')),
     path('api/auth/', include('users.urls')),
